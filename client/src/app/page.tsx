346090384a401f4195967c771ecbf6514895d187
Thomas Dos Anjos <dosanjos.thomas@gmail.com>
2023-08-10 19:06:47 +0200

    refacto  home page

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,5 +1,33 @@
 'use client'
 
+import Image from 'next/image'
+import styled from 'styled-components'
+
+const StyledH1 = styled.h1`
+  color: #333;
+  font-family: Poppins;
+  font-size: 56px;
+  font-style: normal;
+  font-weight: 800;
+  line-height: 73px; /* 130.357% */
+  letter-spacing: -0.56px;
+  width: 50%;
+`
+
 export default function Home() {
-  return <main className='flex  flex-col items-center'></main>
+  return (
+    <main className='flex flex-col items-center px-10'>
+      <div className='flex justify-center '>
+        <div className='flex flex-col pt-10'>
+          <StyledH1>Decentralized Voting for a Trustworthy Democracy</StyledH1>
+          <span>
+            Welcome to our platform where cutting-edge technology meets democratic participation. Explore the world of
+            decentralized identity voting.
+          </span>
+          <button>Get Started</button>
+        </div>
+        <Image src='/svg/home-passport-landing.svg' alt='' width={750} height={750} />
+      </div>
+    </main>
+  )
 }
--- client/src/components/layout/navigation.tsx
+++ client/src/components/layout/navigation.tsx
@@ -2,38 +2,65 @@ import { auth } from '@/services/firebase'
 import { useAppSettingsStore } from '@/store/app-settings'
 import { ConnectKitButton } from 'connectkit'
 import { signOut } from 'firebase/auth'
+import Image from 'next/image'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import toast from 'react-hot-toast'
+import styled from 'styled-components'
+
+const StyledNavTitle = styled.div<{ isCurrent: boolean }>`
+  color: #333;
+  font-family: Poppins;
+  font-size: 24px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+  &:hover {
+    color: var(--color-primary);
+  }
+  padding: ${(props) => (props.isCurrent ? '5px 10px' : 'none')};
+  border: ${(props) => (props.isCurrent ? '2px solid var(--color-primary)' : 'none')};
+  border-radius: ${(props) => (props.isCurrent ? '20px' : 'none')};
+`
 
 function Navigation() {
   const isAdmin = useAppSettingsStore((state) => state.isAdmin)
+  const pathname = usePathname()
 
   return (
-    <nav className='flex justify-between p-6 mb-10 border-b-2'>
+    <nav className='flex justify-between p-6 mb-10'>
       <Link href='/'>
-        <div>EID</div>
+        <div className='flex items-center gap-2'>
+          <Image src='/svg/electid-logo.svg' alt='ElectID Logo' width={50} height={50} />
+          <span className='text-2xl font-bold text-primary'>ElectID</span>
+        </div>
       </Link>
       <div className='flex items-center gap-10'>
-        <Link href='/create-new-id'>
-          <div>Create a new ID</div>
+        <Link href='/'>
+          <StyledNavTitle isCurrent={pathname === '/'}>Home</StyledNavTitle>
         </Link>
-        <ConnectKitButton />
+        <Link href='/create-new-id'>
+          <StyledNavTitle isCurrent={pathname === '/create-new-id'}>Create a new ID</StyledNavTitle>
+        </Link>
 
         <Link href='/admin'>
-          <div>Admin</div>
+          <StyledNavTitle isCurrent={pathname === '/admin'}>Admin</StyledNavTitle>
         </Link>
         {isAdmin && (
-          <div
-            className='cursor-pointer'
-            onClick={() =>
-              signOut(auth)
-                .then(() => useAppSettingsStore.setState({ isAdmin: false }))
-                .catch((err) => toast.error(err.message))
-            }>
-            Log out
-          </div>
+          <StyledNavTitle isCurrent={false}>
+            <div
+              className='cursor-pointer text-base'
+              onClick={() =>
+                signOut(auth)
+                  .then(() => useAppSettingsStore.setState({ isAdmin: false }))
+                  .catch((err) => toast.error(err.message))
+              }>
+              Log out
+            </div>
+          </StyledNavTitle>
         )}
       </div>
+      <ConnectKitButton theme='retro' />
     </nav>
   )
 }
